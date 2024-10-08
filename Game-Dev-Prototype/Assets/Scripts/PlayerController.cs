4849c46d89dbc3c344a86963478a91233b05edfb
Bernadette Wilson <Bernadettewilson60@gmail.com>
2024-10-09 12:45:15 +1300

    cleaned up extra assets and turned fish into prefab

--- Game-Dev-Prototype/Assets/Scripts/PlayerController.cs
+++ Game-Dev-Prototype/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10f;
     public float xRange = 3f;
+
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
